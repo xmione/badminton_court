88503cb54b58d2bcb46c05ff8a923161ce426d77
xmione <solomiosisante@gmail.com>
2025-10-06 01:41:02 +0800

    Fixed error on deleting paid booking.

--- cypress/e2e/booking/delete-paid-booking.cy.js
+++ cypress/e2e/booking/delete-paid-booking.cy.js
@@ -1,8 +1,42 @@
 // cypress/e2e/booking/delete-paid-booking.cy.js
 
 describe('Booking Management', () => {
-  
+
+    let statusId;
+  before(() => {
+    cy.log('BOOKING HAPPY PATH SPEC: Starting admin-login.cy.js before()');
+    cy.showWaitMessage('This will process the Happy Path Booking Test.', 10000);
+
+    cy.showStatusMessage('Resetting Database...', {
+      showSpinner: true,
+      subText: 'Please be wait...'
+    }).then(id => {
+      statusId = id; // Assign the ID returned by showStatusMessage
+    });
+
+    // Reset the database
+    cy.resetDatabase();
+
+    // Create test data for bookings (customers and courts)
+    cy.updateStatusMessage(statusId, 'Creating test data for Bookings...', 'Please be patient...');
+
+    cy.wait(1000) // Add a small wait
+    cy.createBookingTestData();
+
+    // Login as a regular user
+    cy.updateStatusMessage(statusId, 'Logging in as a Regular user...', 'Please be patient...');
+
+    cy.wait(1000) // Add a small wait
+    cy.loginAsRegularUser();
+  });
+
+  beforeEach(() => {
+    cy.log('BOOKING HAPPY PATH SPEC: Starting beforeEach()');
+
+  });
+
   it('should not allow deletion of a paid booking', () => {
+    
     cy.processPayment();     
     cy.deletePaidBooking();
   });
--- cypress/e2e/booking/process-payment.cy.js
+++ cypress/e2e/booking/process-payment.cy.js
@@ -1,7 +1,6 @@
 // cypress/e2e/booking/process-payment.cy.js 
 describe('Booking Management', () => {
-  let statusId; 
-  
+    
   it('should view booking details', () => {
     cy.processPayment();
   });
